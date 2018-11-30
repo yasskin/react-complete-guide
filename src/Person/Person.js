bdcc5ffa1b34895c70085b9594ed4cf5d4db87e1
Noah Yasskin <noahyasskin@gmail.com>
2018-11-29 18:43:58 -0800

    add lessons through section 4

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Person.css';
 
-const person = () => {
-  return <p>I'm a Person and I am Math.floor(Math.random() * 30) years old!</p>
-}
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+  </div>
+  )
+};
 
 export default person;
